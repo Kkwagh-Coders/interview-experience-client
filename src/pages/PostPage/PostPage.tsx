77547ad24e1364ee901de6f7d527f7b57de616c7
Dhruv Chavda <dhruvchavda10i@gmail.com>
2023-02-08 23:43:07 +0530

    feat(user auth) created post-page-folder

---
+++ src/pages/PostPage/PostPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NotFound.module.css';
+
+function PostPage() {
+  return (
+    <div className={styles.PostPage}>
+      <h1>post page</h1>
+    </div>
+  );
+}
+
+export default PostPage;
