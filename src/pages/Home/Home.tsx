edb55a7d79bceefe158f3468d80d6e4825089a88
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-02-09 14:28:14 +0530

    feature(home page): Created Banner for the Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
-import styles from './Home.module.css';
 import { useAppSelector } from '../../redux/store';
+import styles from './Home.module.css';
 
 function Home() {
   const userState = useAppSelector((state) => state.userState);
@@ -11,11 +11,40 @@ function Home() {
 
   return (
     <div className={styles.Home}>
-      <h1>Home Page</h1>
+      <section className={styles.hero}>
+        <div className="container">
+          <h1 className={styles.title}>
+            <span>A Friendly Tool to</span>
+            <span>Crack Interview</span>
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            molestiae obcaecati atque distinctio odit a aperiam? Porro eveniet
+            in fugiat? Consectetur similique reiciendis eum qui eligendi quasi
+            blanditiis vitae fugit!
+          </p>
+          <div className={styles.heroActionButtons}>
+            <a
+              href="#recentPosts"
+              className={`default-button ${styles.exploreButton}`}
+            >
+              Explore
+            </a>
+            <a
+              href="#aim"
+              className={`default-button default-outline-button ${styles.aimButton}`}
+            >
+              Our Aim
+            </a>
+          </div>
+        </div>
+      </section>
       <h2>
         Role:
         {userRole}
       </h2>
+
+      <section id="aim">this is mey</section>
     </div>
   );
 }
