9b6706950af71761fa631b61bb3572f3983e8f59
AlgorithmChopda <vinaychopda4@gmail.com>
2023-02-11 00:35:49 +0530

    Feat: added the filter tab

--- src/pages/PostList/PostList.tsx
+++ src/pages/PostList/PostList.tsx
@@ -80,11 +80,11 @@ function PostList() {
                 <span> 200</span>
               </div>
             </div>
-            <div className={styles.postButtonContainer}>
+            {/* <div className={styles.postButtonContainer}>
               <button type="button" className={styles.postButton}>
                 READ
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className={styles.writeContainer}>
@@ -94,7 +94,79 @@ function PostList() {
           </button>
         </div>
       </section>
-      <div>body</div>
+      <section className={styles.posts} id="recentPosts">
+        <div className="container">
+          <h2>
+            <span>Recent Experiences </span>
+          </h2>
+
+          <div className={styles.filter}>
+            <div className={styles.filterSort}>Sort By</div>
+            <div className={styles.filterElement}>Job Role</div>
+            <div className={styles.filterElement}>Company</div>
+            <div className={styles.filterElement}>Job Type</div>
+            <div className={styles.searchBar}>
+              <form role="search">
+                <input
+                  id="search"
+                  type="search"
+                  placeholder="Search..."
+                  required
+                />
+                <button type="submit">Go</button>
+              </form>
+            </div>
+          </div>
+
+          <div className={styles.postsContainer}>
+            <div className={styles.interviewPost}>
+              <div className={styles.upvote}>Upvote</div>
+              <div>
+                <span className={styles.postDomain}>Experience</span>
+                <h3 className={styles.postTitle}>My FinIQ Experience</h3>
+                <p className={styles.postDescription}>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Fugiat pariatur, quibusdam iusto officiis aliquam
+                  reprehenderit tenetur...
+                </p>
+                <p className={styles.postAuthor}>Rama works</p>
+                <div className={styles.postMoreDetail}>
+                  <span>Feb 9, 2022</span>
+                  <span>51 Likes</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles.interviewPost}>
+              <span className={styles.postDomain}>Experience</span>
+              <h3 className={styles.postTitle}>My FinIQ Experience</h3>
+              <p className={styles.postDescription}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+                pariatur, quibusdam iusto officiis aliquam reprehenderit
+                tenetur...
+              </p>
+              <p className={styles.postAuthor}>Rama works</p>
+              <div className={styles.postMoreDetail}>
+                <span>Feb 9, 2022</span>
+                <span>51 Likes</span>
+              </div>
+            </div>
+            <div className={styles.interviewPost}>
+              <span className={styles.postDomain}>Experience</span>
+              <h3 className={styles.postTitle}>My FinIQ Experience</h3>
+              <p className={styles.postDescription}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+                pariatur, quibusdam iusto officiis aliquam reprehenderit
+                tenetur...
+              </p>
+              <p className={styles.postAuthor}>Rama works</p>
+              <div className={styles.postMoreDetail}>
+                <span>Feb 9, 2022</span>
+                <span>51 Likes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
