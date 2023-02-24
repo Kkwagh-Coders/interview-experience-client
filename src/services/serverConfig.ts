fede2a1450fa84229dea11f37961d6c1750aab9f
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-02-24 11:30:31 +0530

    fix(server config): Renamed the env to match with react

--- src/services/serverConfig.ts
+++ src/services/serverConfig.ts
@@ -1,4 +1,4 @@
 export const LATENCY_IN_SECONDS = 1;
 
 // eslint-disable-next-line prefer-destructuring
-export const BASE_API_URL = process.env.BASE_API_URL;
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
