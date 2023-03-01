25639d14b9860b60003732c9303a1676087cebe5
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-03-01 20:56:35 +0530

    fix(postlist): Added filters ui back

--- src/pages/PostList/PostList.tsx
+++ src/pages/PostList/PostList.tsx
@@ -34,21 +34,92 @@ function PostList() {
   return (
     <div className={styles.PostList}>
       <section className={styles.posts} id="recentPosts">
-        {data?.pages
-          .flatMap((page) => page.data)
-          .map((post) => (
-            <PostListElement post={post} key={post._id} />
-          ))}
+        <div className="container">
+          <h2>
+            <span>Recent Experiences</span>
+          </h2>
 
-        <div>
-          <button
-            type="button"
-            className={`default-button ${styles.nextPageButton}`}
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}
-          >
-            {buttonText}
-          </button>
+          <div className={styles.filter}>
+            <div className={styles.searchBar}>
+              <input
+                type="text"
+                className={styles.searchBarInput}
+                placeholder="Search..."
+              />
+            </div>
+
+            <div className={styles.filtersContainer}>
+              <div className={styles.filterInput}>
+                <label htmlFor="domain">
+                  <select name="domain" className={styles.inputField}>
+                    <option value="1">Sort By</option>
+                    <option value="2">Newest</option>
+                    <option value="3">Oldest</option>
+                    <option value="4">Top Voted</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className={styles.filterInput}>
+                <label htmlFor="type">
+                  <select name="type" className={styles.inputField}>
+                    <option value="1">Article Type</option>
+                    <option value="2">Experience</option>
+                    <option value="3">Discussion</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className={styles.filterInput}>
+                <label htmlFor="domain">
+                  <select name="domain" className={styles.inputField}>
+                    <option value="1">Job Role</option>
+                    <option value="2">Software Developer</option>
+                    <option value="3">FullStack-Developer</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className={styles.filterInput}>
+                <label htmlFor="domain">
+                  <select name="domain" className={styles.inputField}>
+                    <option value="1">Company</option>
+                    <option value="2">Amazon</option>
+                    <option value="3">Google</option>
+                    <option value="4">Meta</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className={styles.filterInput}>
+                <label htmlFor="domain">
+                  <select name="domain" className={styles.inputField}>
+                    <option value="1">Rating</option>
+                    <option value="2">1 Star</option>
+                    <option value="3">2 Star</option>
+                    <option value="4">3 Star and above</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {data?.pages
+            .flatMap((page) => page.data)
+            .map((post) => (
+              <PostListElement post={post} key={post._id} />
+            ))}
+
+          <div>
+            <button
+              type="button"
+              className={`default-button ${styles.nextPageButton}`}
+              onClick={() => fetchNextPage()}
+              disabled={!hasNextPage || isFetchingNextPage}
+            >
+              {buttonText}
+            </button>
+          </div>
         </div>
       </section>
     </div>
