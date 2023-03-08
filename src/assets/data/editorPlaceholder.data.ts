35f51abd5456fa5cdc153ec3db21b5bac2544266
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-03-08 19:18:27 +0530

    feat(post form): Added placeholder for the post content

---
+++ src/assets/data/editorPlaceholder.data.ts
@@ -0,0 +1,6 @@
+const editorPlaceholder = `- Interview date - an approximate date is fine.
+- The interview process and format of each interview in Brief and Result of Interview
+- Your thoughts and anything else you'd like to add, e.g. culture, office, perks, lunch, accommodations, etc.
+`;
+
+export default editorPlaceholder;
