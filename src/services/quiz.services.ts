657612ff16e6510d68c100a44a4c5978c5671c58
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-03-19 10:41:24 +0530

    feat(quiz question form): Created a quiz form with dummy api

--- src/services/quiz.services.ts
+++ src/services/quiz.services.ts
@@ -1,1 +1,15 @@
+import { QuizFormData } from '../types/quiz.types';
+import fakeRequest from './fakeRequest';
+
+export const createQuizQuestion = (data: QuizFormData) => {
+  console.log(data);
+  return fakeRequest(
+    data,
+    { message: 'Question Added Successfully' },
+    false,
+  ).then((response: any) => response.expectedResponse) as Promise<{
+    message: string;
+  }>;
+};
+
 export default {};
---
+++ src/types/quiz.types.ts
@@ -0,0 +1,9 @@
+export type QuizFormData = {
+  question: string;
+  answer: string;
+  wrongOption1: string;
+  wrongOption2: string;
+  wrongOption3: string;
+  topic: string;
+  difficulty: number;
+};
