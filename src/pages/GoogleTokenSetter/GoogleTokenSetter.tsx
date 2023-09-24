85c2ade8bdf3514016310bd97a8e6badea7906d5
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-09-24 19:41:50 +0530

    feat: Added loading screen when token is being set

--- src/pages/GoogleTokenSetter/GoogleTokenSetter.tsx
+++ src/pages/GoogleTokenSetter/GoogleTokenSetter.tsx
@@ -2,6 +2,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { setUserToken } from '../../services/user.services';
+import Loading from '../Loading/Loading';
 
 function GoogleTokenSetter() {
   const { token } = useParams();
@@ -21,7 +22,7 @@ function GoogleTokenSetter() {
     handleGoogleTokenSetter();
   }, [token, navigate, queryClient]);
 
-  return <div>Google Token Verification</div>;
+  return <Loading />;
 }
 
 export default GoogleTokenSetter;
