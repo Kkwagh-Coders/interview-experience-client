4265b6be5397f34e3c9c544c11bc877f3771a915
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-09-24 19:25:55 +0530

    feat: Added refetch of queries

--- src/pages/GoogleTokenSetter/GoogleTokenSetter.tsx
+++ src/pages/GoogleTokenSetter/GoogleTokenSetter.tsx
@@ -1,21 +1,25 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { setUserToken } from '../../services/user.services';
 
 function GoogleTokenSetter() {
-  const navigate = useNavigate();
   const { token } = useParams();
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     if (!token) return;
 
     const handleGoogleTokenSetter = async () => {
       await setUserToken(token);
+      queryClient.refetchQueries(['user-status']);
       navigate('/');
     };
 
     handleGoogleTokenSetter();
-  }, [navigate, token]);
+  }, [token, navigate, queryClient]);
 
   return <div>Google Token Verification</div>;
 }
